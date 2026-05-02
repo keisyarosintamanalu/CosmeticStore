1e31d4fc270654ad0a8c1d5d0bbcb0f7eca14bf2
keisyarosintamanalu <keisyarosintamanalu@gmail.com>
2026-05-02 21:42:27 +0700

    update

--- Config/Database.php
+++ Config/Database.php
@@ -1,14 +1,14 @@
 <?php
 class Database {
 
-    private $host = "localhost";
-    private $db   = "cosmetik_store"; // SESUAI TERMINAL KAMU
+    private $alamat = "127.0.0.1";
+    private $db   = "cosmetik_store_Keisya"; 
     private $user = "root";
     private $pass = "";
 
     public function connect() {
         try {
-            $conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
+            $conn = new PDO("mysql:host=$this->alamat;dbname=$this->db", $this->user, $this->pass);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch(PDOException $e) {
